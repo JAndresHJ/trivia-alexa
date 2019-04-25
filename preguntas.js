9502a9bfec89e6cd9a24cb64d436c8c87b9e9223
emilio-nuno <38873069+emilio-nuno@users.noreply.github.com>
2019-04-24 22:16:23 -0500

    Added trivia questions
    They come with pre-loaded answers

---
+++ preguntas.js
@@ -0,0 +1,79 @@
+const Matematicas = [{
+    "Question": "¿Cómo se calcula el área de un rectángulo?",
+    "Answers":
+            {'a': "Multiplicando la base por la altura",
+            'b': "Sumando la base y la altura",
+            'c': "Dividiendo la base y la altura",
+            'd': "Restando la base y la altura"
+            },
+    "Correct": 'a'},
+    {
+    "Question": "¿Qué es la secuencia de Fibonacci?",
+    "Answers":
+            {'a': "Una secuencia donde el elemento es generado por la suma de los dos anteriores",
+            'b': "Una suma donde hay un incremento constante",
+            'c': "Una serie aritmética",
+            'd': "Una serie donde el elemento es generado por la resta de los dos anteriores"
+            },
+    "Correct": 'a'}
+]
+
+const Biologia = [{
+    "Question": "¿Qué es la biología?",
+    "Answers":
+            {'a': "Una ciencia que estudia las computadoras",
+            'b': "Es la ciencia que estudia a los seres vivos",
+            'c': "Es una ciencia que estudia fenómenos naturales",
+            'd': "Es una ciencia que estudia los minerales"
+            },
+    "Correct": 'b'},
+    {
+    "Question": "¿Los virus y las bacterias están vivas?",
+    "Answers":
+            {'a': "Los virus sí, bacterias no",
+            'b': "Las bacterias sí, los virus no",
+            'c': "Ninguno está vivo",
+            'd': "No sé"
+            },
+    "Correct": 'c'}
+]
+
+const Geometria = [{
+    "Question": "¿Qué estudia la geometría?",
+    "Answers":
+            {'a': "Los números y su naturaleza",
+            'b': "Los cuerpos celestiales",
+            'c': "La relación entre los números",
+            'd': "Las proporciones y singularidades de distintas figuras en un plano o el espacio"
+            },
+    "Correct": 'd'},
+    {
+    "Question": "¿Cuáles son las características de las rectas?",
+    "Answers":
+            {'a': "Se extienden en una misma dirección y son de una dimensión",
+            'b': "Se extienden en dos direcciones y son de dos dimensiones",
+            'c': "Se extienden en una dirección y son multidimensionales",
+            'd': "No se extienden en ninguna dirección y son unidimensionales"
+            },
+    "Correct": 'a'}
+]
+
+const Computacion = [{
+    "Question": "¿Qué es la programación?",
+    "Answers":
+            {'a': "Decirle a una computadora qué hacer a base de un set de instrucciones",
+            'b': "Calcular el área de un cuadrado",
+            'c': "Arreglar una computadora",
+            'd': "Decirle a una computadora qué hacer a base de un set infinito de instrucciones"
+            },
+    "Correct": 'a'},
+    {
+    "Question": "¿Qué es una variable?",
+    "Answers":
+            {'a': "Una función",
+            'b': "Un parámetro",
+            'c': "Una manera de etiquetar información con un nombre descriptivo",
+            'd': "Un compilador"
+            },
+    "Correct": 'c'}
+]
