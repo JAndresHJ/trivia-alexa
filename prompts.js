1e6562e0728fc43feea3e962e2b7ee610132d3c1
emilio-nuno <38873069+emilio-nuno@users.noreply.github.com>
2019-04-24 23:33:30 -0500

    Added prompts for each intent
    For a more interesting Alexa experience!

---
+++ prompts.js
@@ -0,0 +1,63 @@
+const stringsRulesIntent = [
+    speechText = ["Tienes que decir el nombre de tu equipo", "Antes de comenzar"],
+    helpPrompt = ["Antes de comenzar", "Deberás decirme el nombre de tu equipo"]
+]
+const stringsStartGameIntent = [
+    speechText = ["¡Comencemos con la trivia!", "¡Es hora de comenzar!"],
+    helpPrompt = ["Puedes elegir entre escuchar las reglas o jugar", "Quieres jugar o escuchar las reglas?"]
+]
+
+const stringsTopicIntent = [
+    speechText = ["Por favor elija uno de los siguientes temas:", "Antes de comenzar, elija un tema de los disponibles:"],
+    helpPrompt = ["1. Computación, 2. Geometría, 3. Biología, 4. Matemáticas", "1. Computación, 2. Geometría, 3. Biología, 4. Matemáticas"]
+]
+
+const stringsPlayersQuantityIntent = [
+    speechText = ["Debes decir tu nombre", "Es menester proporcionarme el nombre de tu equipo"],
+    helpPrompt = ["Antes de comenzar", "Previo a comenzar el juego"]
+]
+
+const stringsAnswersIntent = [
+    speechText = ["Dame tu respuesta", "Necesito su respuesta"],
+    helpPrompt = ["Por favor", "Por favor démela"]
+]
+
+const stringsScoreIntent = [
+    speechText = ["Tu calificación es: ", "La calificación de tu equipo es: "],
+    helpPrompt = ["! Sigue así", "! Sigan así"]
+]
+
+const stringsEndGameIntent = [
+    speechText = ["Ya te vas?", "Adiós!"],
+    helpPrompt = ["Buena suerte!", "Vuelve pronto"]
+]
+
+const stringsPlayAgainIntent = [
+    speechText = ["Quieres jugar otra vez?", "Otra?"],
+    helpPrompt = ["Vamos, pues!", "Tú puedes!"]
+]
+
+const stringsAddTopicIntent = [
+    speechText = ["", ""],
+    helpPrompt = ["", ""]
+]
+
+const stringsAddQuestionIntent = [
+    speechText = ["", ""],
+    helpPrompt = ["", ""]
+]
+
+const stringsAddAnswerIntent = [
+    speechText = ["", ""],
+    helpPrompt = ["", ""]
+]
+
+const stringsTopicControllerIntent = [
+    speechText = ["", ""],
+    helpPrompt = ["", ""]
+]
+
+const stringsCloseTopicIntent = [
+    speechText = ["", ""],
+    helpPrompt = ["", ""]
+]
